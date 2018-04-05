96b7ad56d759bfbb90e88cc338612d43a7975f62
ColinCheng5887 <yyan7@student.unimelb.edu.au>
2018-04-05 16:31:48 +1000

    Colin

--- src/activitystreamer/Server.java
+++ src/activitystreamer/Server.java
@@ -1,38 +1,28 @@
 package activitystreamer;
 
 import activitystreamer.server.CommandServerHelper;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import activitystreamer.server.Control;
 
 public class Server {
-   private static final Logger log = LogManager.getLogger();
+    private static final Logger log = LogManager.getLogger();
 
-   public static void main(String[] args) {
-      log.info("reading command line options");
-      CommandServerHelper.SetReadCommands(args);
+    public static void main(String[] args) {
+        log.info("reading command line options");
+        CommandServerHelper.SetReadCommands(args);
 
-      JsonObject _object = new JsonObject();
-      _object.addProperty("name", "Ruby");
-      _object.addProperty("IQ", 1.5);
-      String _content=_object.toString();
-      JsonParser parser = new JsonParser();
-
-      JsonObject _obj = (JsonObject) parser.parse(_content);
-
-      log.info("starting server");
-      final Control c = Control.getInstance();
-      // the following shutdown hook doesn't really work, it doesn't give us enough time to
-      // cleanup all of our connections before the jvm is terminated.
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-         public void run() {
-            c.setTerm(true);
-            c.interrupt();
-         }
-      });
-   }
+        log.info("starting server");
+        final Control c = Control.getInstance();
+        // the following shutdown hook doesn't really work, it doesn't give us enough time to
+        // cleanup all of our connections before the jvm is terminated.
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                c.setTerm(true);
+                c.interrupt();
+            }
+        });
+    }
 
 }
--- src/activitystreamer/util/JsonHelper.java
+++ src/activitystreamer/util/JsonHelper.java
@@ -1,17 +1,35 @@
 package activitystreamer.util;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+/**
+ * Utility class used to for JSON convert
+ */
 public class JsonHelper {
-   public static String ToJson(JsonObject argObject) {
-      return argObject.toString();
-   }
 
-   public class TestObject {
-      public String name;
-      public int age;
+    private static JsonParser parser = new JsonParser();
 
-   }
+
+    /**
+     * Convert a JsonObject object to string
+     *
+     * @param argObject The object to be converted
+     * @return
+     */
+    public static String ToJson(JsonObject argObject) {
+        String _content = argObject.toString();
+        return _content;
+    }
+
+    /**
+     * Convert string to a JsonObject object
+     *
+     * @param argContent Json data string
+     * @return
+     */
+    public static JsonObject ConvertToObject(String argContent) {
+        JsonObject _obj = (JsonObject) parser.parse(argContent);
+        return _obj;
+    }
 }
-
-
