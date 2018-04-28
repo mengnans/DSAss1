34bfffee3ae2ab631a6140aaae1668ca41b54377
ColinCheng5887 <yyan7@student.unimelb.edu.au>
2018-04-28 15:00:44 +1000

    Client 1 bug solved

--- src/activitystreamer/client/ClientProcessor.java
+++ src/activitystreamer/client/ClientProcessor.java
@@ -33,30 +33,41 @@ public class ClientProcessor {
      */
     public static void ProcessNetworkMessage(JsonObject argJsonObject) {
         switch (argJsonObject.get("command").toString()) {
+            // I have no idea why there is two " at the begin and the end of the string
+            // I spent half hour and this is the only solution works
+            // That's why I hate java
             case "INVALID_MESSAGE":
+            case "\"INVALID_MESSAGE\"":
                 apiHelper.SetDisplayMessage(argJsonObject.get("info").toString());
                 break;
             case "AUTHENTICATION_FAIL":
+            case "\"AUTHENTICATION_FAIL\"":
                 apiHelper.CloseConnection();
                 break;
             case "LOGIN_SUCCESS":
+            case "\"LOGIN_SUCCESS\"":
                 apiHelper.SetDisplayMessage(argJsonObject.get("info").toString());
                 break;
             case "LOGIN_FAILED":
+            case "\"LOGIN_FAILED\"":
                 apiHelper.SetDisplayMessage("Failed to log in: " + argJsonObject.get("info"));
                 apiHelper.CloseConnection();
                 break;
             case "ACTIVITY_BROADCAST":
+            case "\"ACTIVITY_BROADCAST\"":
                 apiHelper.SetDisplayMessage(argJsonObject);
                 break;
             case "REDIRECT":
+            case "\"REDIRECT\"":
                 //这里功能不全，除了显示结果，还应该解析数据，并且连接到新的服务器上
                 apiHelper.SetDisplayMessage("Redirected to host:" + argJsonObject.get("hostname") + " prot:" + argJsonObject.get("port"));
                 break;
             case "REGISTER_FAILED":
+            case "\"REGISTER_FAILED\"":
                 apiHelper.SetDisplayMessage("Failed to register because " + argJsonObject.get("info"));
                 break;
             case "REGISTER_SUCCESS":
+            case "\"REGISTER_SUCCESS\"":
                 //注册成功了是不是应该直接登陆上？
                 apiHelper.SetDisplayMessage(argJsonObject.get("info").toString());
                 break;
@@ -73,6 +84,9 @@ public class ClientProcessor {
         Object _message = argJsonObject.get("command");
         String _command = _message.toString();
         switch (_command) {
+            // I have no idea why there is two " at the begin and the end of the string
+            // I spent half hour and this is the only solution works
+            // That's why I hate java
             case "REGISTER":
             case "\"REGISTER\"":
                 apiHelper.SendMessage(argJsonObject);
