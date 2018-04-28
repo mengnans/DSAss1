31e3e3855b2f9188fc14343e6fa348b0ad2858d0
ColinCheng5887 <yyan7@student.unimelb.edu.au>
2018-04-28 14:38:55 +1000

    Client 1 bug solved

--- src/activitystreamer/util/Settings.java
+++ src/activitystreamer/util/Settings.java
@@ -16,29 +16,35 @@ public class Settings {
     private static String localHostname = "localhost";
     private static String remoteHostname = null;
     private static int remotePort = 3780;
-    private static String remoteServerSecret=null;
+    private static String remoteServerSecret = null;
     private static int activityInterval = 5000; // milliseconds
     private static String secret = null;
     private static String username = "anonymous";
 
-    public static void setServerID(String ID){
-        serverID =serverID;
+    public static void setServerID(String ID) {
+        serverID = serverID;
     }
-    public static String getServerID(){
+
+    public static String getServerID() {
         return serverID;
     }
-    public static void setServerSecret(String Ssecret){
-        serverSecret=Ssecret;
+
+    public static void setServerSecret(String Ssecret) {
+        serverSecret = Ssecret;
     }
-    public static String getServerSecret(){
+
+    public static String getServerSecret() {
         return serverSecret;
     }
-    public static void setRemoteServerSecret(String rs){
-        remoteServerSecret =rs;
+
+    public static void setRemoteServerSecret(String rs) {
+        remoteServerSecret = rs;
     }
-    public static String getRemoteServerSecret(){
+
+    public static String getRemoteServerSecret() {
         return remoteServerSecret;
     }
+
     public static int getLocalPort() {
         return localPort;
     }
@@ -116,6 +122,8 @@ public class Settings {
         return new BigInteger(130, random).toString(32);
     }
 
-    public static String nextServerID() {return new BigInteger(130, random).toString(32);}
+    public static String nextServerID() {
+        return new BigInteger(130, random).toString(32);
+    }
 
 }
